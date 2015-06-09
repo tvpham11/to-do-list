c5f6cb38b6c6ea19c8ee6ffe4424c0bcf20cea0c
Vi Pham <tvpham11@gmail.com>
2015-06-08 23:19:02 -0400

    refactored code and added prototypal methods, delete function is still extremely buggy. maybe more so

--- app/js/main.js
+++ app/js/main.js
@@ -1,33 +1,148 @@
+// (function() {
 // 'use strict';
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Task constructor ~~~~~~~~~~~~~~~~~~~~~~~~~~
-var Todo = function (options) {
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Task constructor & prototype ~~~~~~~~~~~~~~~~~~~~~~~~~~
+var Task = function (options) {
   var args = options || {};
 
   this.task = args.task;
-  this.status = 'Open';
+  this.status = 'Open'; // value can be 'Open' or 'Closed'
   this.id = _.random(200, 6000); // Added with Tim on Mon., Jun 8 - Idea is to have
                                  // unique ID for each task
 };
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~
+Task.prototype.genHTML = function (divID, checked, tasktext, deletebtnID) {
+  return '<li class="taskli" id="' + this.id + '"><div class="taskitem" id="' + divID + '">' +
+  '<input type="checkbox" ' + checked +'>' + tasktext + '</div><div class="deletebtn" id="' +
+  deletebtnID + '><a href="#"><i class="fa fa-trash-o"></i></a></div></li>';
+};
 
-function taskCount(storagebin, ulID, status) {
-  if (storagebin.length > 0) {
-    $(ulID).html('Number of ' + status + ' tasks: ' + storagebin.length);
+Task.prototype.delete = function (total, ulID, status) {
+  $('.deletebtn').click(function (event) {
+    event.preventDefault();
+    var $taskID = $(this).parent().attr('id');
+    storageBin = _.without(storageBin, _.find(storageBin, { id: Number($taskID) }));
+    $(this).closest('li').remove();
+    taskCount();
+    displayCount(total, ulID, status);
+  });
+};
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Global variables ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+var openTotal, closedTotal;
+
+var storageBin = [];
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~
+function taskCount() {
+  openTotal = 0;
+  closedTotal = 0;
+  _.each(storageBin, function(task) {
+    if (task.status === 'Open') {
+      return openTotal++;
+    } else if (task.status === 'Closed') {
+      return closedTotal++;
+    }
+  });
+}
+
+function displayCount(total, ulID, status) {
+  if (total > 0) {
+    $(ulID).html('Number of ' + status + ' tasks: ' + total);
   } else {
     $(ulID).html('');
   }
 }
 
-function deleteTask (delbtn, storagebin, task, taskcountfunc, ulID, status) {
-  $('.taskli').on('click', delbtn, function (event) {
-    event.preventDefault();
-    storagebin.splice($.inArray(task, storagebin), 1);
-    $(this).closest('li').remove();
-    taskcountfunc(storagebin, ulID, status);
-  });
-}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Adding to task list ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Set up submit
+// On submit, create a new instance
+$('#addTask').on('submit', function (event) {
+
+  event.preventDefault();
+  var $taskText = $('#taskText').val();
+  var taskInstance = new Task ({task: taskText});
+
+  // Store it for later
+  storageBin.push(taskInstance);
+
+  // Display on page
+  var opentaskhtml = taskInstance.genHTML("opentask", "", $taskText, "deleteopen");
+  $('#opentasks').append(opentaskhtml);
+
+  // Reset form
+  this.reset();
+
+  // Open task count
+  taskCount();
+  displayCount(openTotal, '#opencount', 'open');
+
+  // Delete task
+  taskInstance.delete(openTotal, '#opencount', 'open');
+
+});
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Change task to completed ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+$('#opentasks').on('click', '.taskitem', function(event) {
+  event.preventDefault();
+
+  // Mark tasks completed
+  $(this).toggle('complete');
+  $(this).next('.deletebtn').remove();
+
+  var $cTask = $(this).text();
+
+  // Grab task that was clicked on
+  var $taskID = $(this).parent().attr('id'); // Added with Tim, used to search for task w/ unique ID
+  var taskToClose = _.find(storageBin, { id: Number($taskID) }); // Added with Tim
+
+  taskToClose.status = 'Closed';
+
+  // Update task counts
+  taskCount();
+  displayCount(openTotal,'#opencount', 'open');
+  displayCount(closedTotal,'#closedcount', 'closed');
+
+  // Display on page
+  var closedtaskhtml = taskToClose.genHTML("closedtask", "checked", $cTask, "deleteclosed");
+  $('#closedtasks').append(closedtaskhtml);
+
+  // Delete task
+  taskToClose.delete(closedTotal, '#closedcount', 'closed');
+
+});
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Toggle from Completed to Open ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+$('#closedtasks').on('click', '.taskitem', function(event) {
+  event.preventDefault();
+
+  var $tTask = $(this).text();
+  var $taskID = $(this).parent().attr('id');
+
+   // Remove from completed task list
+  $(this).closest('li').remove();
+
+  // Grab task that was clicked on and change status
+  var taskToEdit = _.find(storageBin, { id: Number($taskID) });
+  taskToEdit.status = 'Open';
+
+  // Display in open task list
+  var reopentaskhtml = taskToEdit.genHTML("opentask", "", $tTask, "deleteopen");
+  $('#opentasks').append(reopentaskhtml);
+
+  // Update task counts
+  taskCount();
+  displayCount(openTotal,'#opencount', 'open');
+  displayCount(closedTotal,'#closedcount', 'closed');
+
+  // Delete reopened task
+  taskToEdit.delete(openTotal, '#opencount', 'open');
+
+});
 
 /*
 Tim's solution:
@@ -42,114 +157,4 @@ $('#removeTask').on('click', function(event) {
   });
 });
 */
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Task arrays ~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-var openStorageBin = [];
-var closedStorageBin = [];
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Adding to task list ~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// Set up submit
-// On submit, create a new instance
-$('#addTask').on('submit', function (event) {
-
-  event.preventDefault();
-  var taskText = $('#taskText').val();
-  var taskInstance = new Todo ({task: taskText});
-
-  // Store it for later
-  openStorageBin.push(taskInstance);
-
-  // Display on page
-  var opentaskhtml = '<li class="taskli" id="' + taskInstance.id + '"><div class="taskitem" id="opentask">' +
-  '<input type="checkbox">' + taskText + '</div><div class="deletebtn" id="deleteopen"><a href="#">' +
-  '<i class="fa fa-trash-o"></i></a></li>';
-  $('#opentasks').append(opentaskhtml);
-
-  // Reset form
-  this.reset();
-
-  // Open task count
-  taskCount(openStorageBin,'#opencount', 'open');
-
-  // Delete open task
-  deleteTask('#deleteopen a', openStorageBin, taskInstance, taskCount,'#opencount', 'open');
-});
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Change task to completed ~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// Mark tasks completed
-$('#opentasks').on('click', 'li', function(event) {
-
-  // grab task that was clicked on
-  // mark task as completed
-  $(this).toggle('complete');
-  $(this).next('.deletebtn').remove();
-
-  // var cTask = $(this).text(); // Commented due to using ID random number instead of text
-
-  var taskID = $(this).attr('id'); // Added with Tim
-  // var taskToClose = _.find(openStorageBin, { task: cTask }); // See line 76
-  var taskToClose = _.find(openStorageBin, { id: Number(taskID) }); // Added with Tim
-
-  taskToClose.status = 'Closed';
-
-  // Remove from openStorageBin
-  openStorageBin.splice($.inArray(taskToClose, openStorageBin), 1);
-
-  // Store for later
-  closedStorageBin.push(taskToClose);
-
-  // Update open task count
-  taskCount(openStorageBin,'#opencount', 'open');
-
-  // Closed task count
-  taskCount(closedStorageBin,'#closedcount', 'closed');
-
-  // Display on page
-  var closedtaskhtml = '<li class="taskli"><div class="taskitem" id="closedtask">' +
-  '<input type="checkbox" checked>' + cTask + '</div><div class="deletebtn" id="deleteclosed">' +
-  '<a href="#"><i class="fa fa-trash-o"></i></a></li>';
-  $('#closedtasks').append(closedtaskhtml);
-
-  //  Delete closed task
-  deleteTask('#deleteclosed a', closedStorageBin, taskToClose, taskCount,'#closedcount', 'closed');
-
-});
-
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~ Toggle from Completed to Open ~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-$('#closedtasks').on('click', 'li', function (event) {
-  event.preventDefault();
-  $(this).toggleClass('complete');
-  $(this).next('.deletebtn').remove();
-
-  var tTask = $(this).text();
-
-  var taskToEdit = _.find(closedStorageBin, { task: tTask });
-
-  taskToEdit.status = 'Open';
-
-  // Store for later
-  openStorageBin.push(taskToEdit);
-
-  // Remove from closedStorageBin
-  closedStorageBin.splice($.inArray(taskToEdit, closedStorageBin), 1);
-  $(this).remove();
-
-  // Update closed task count
-  taskCount(closedStorageBin,'#closedcount', 'closed');
-
-  // Update open task count
-  taskCount(openStorageBin,'#opencount', 'open');
-
-  // Display on page
-  var reopentaskhtml = '<li class="taskli"><div class="taskitem" id="opentask">' +
-  '<input type="checkbox">' + tTask + '</div><div class="deletebtn" id="deleteopen">' +
-  '<a href="#"><i class="fa fa-trash-o"></i></a></div></li>';
-  $('#opentasks').append(reopentaskhtml);
-
-  // Delete re-opened task
-  deleteTask('#deleteopen a', openStorageBin, taskToEdit, taskCount,'#opencount', 'open');
-
-});
+// });
